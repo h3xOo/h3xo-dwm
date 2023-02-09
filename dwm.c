70ef7689842c7062bb6e54cb845713895a12ccdb
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-02-09 12:55:00 +0100

    Revert broken patch in grabkeys()

--- dwm.c
+++ dwm.c
@@ -805,27 +805,17 @@ void grabbuttons(Client *c, int focused) {
 void grabkeys(void) {
     updatenumlockmask();
     {
-        unsigned int i, j, k;
+        unsigned int i, j;
         unsigned int modifiers[] = {0, LockMask, numlockmask,
                                     numlockmask | LockMask};
-        int start, end, skip;
-        KeySym *syms;
+        KeyCode code;
 
         XUngrabKey(dpy, AnyKey, AnyModifier, root);
-        XDisplayKeycodes(dpy, &start, &end);
-        syms = XGetKeyboardMapping(dpy, start, end - start + 1, &skip);
-        if (!syms)
-            return;
-        for (k = start; k <= end; k++)
-            for (i = 0; i < LENGTH(keys); i++)
-                /* skip modifier codes, we do that ourselves */
-                if (keys[i].keysym == syms[(k - start) * skip])
-                    for (j = 0; i < LENGTH(modifiers); j++)
-                        XGrabKey(dpy, k,
-                                 keys[i].mod | modifiers[i],
-                                 root, True,
-                                 GrabModeAsync, GrabModeAsync);
-        XFree(syms);
+        for (i = 0; i < LENGTH(keys); i++)
+            if ((code = XKeysymToKeycode(dpy, keys[i].keysym)))
+                for (j = 0; j < LENGTH(modifiers); j++)
+                    XGrabKey(dpy, code, keys[i].mod | modifiers[j], root, True,
+                             GrabModeAsync, GrabModeAsync);
     }
 }
 
