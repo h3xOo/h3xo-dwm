bfcced40e9d029402951e802b0322eca1d3d9d81
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-04-15 21:09:48 +0200

    Update config.h

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1; /* 0 means bottom bar */
 
 static const char* fonts[] = {
-    "Source Code Pro:size=12",
+    "SauceCodePro Nerd Font:size=12",
     "JoyPixels:pixelsize=12:antialias=true:autohint=true"
 };
 static const char normbgcolor[] = "#222222";
@@ -68,11 +68,9 @@ static const Rule rules[] = {
     /* class            instance    title           tags mask    isfloating
        isterminal  noswallow   monitor */
     { "discord", NULL, NULL, 1 << 1, 0, 0, 0, -1 },
-    { "Code", NULL, NULL, 1 << 2, 0, 0, 0, -1 },
-    { "jetbrains-idea", NULL, NULL, 1 << 3, 0, 0, 0, -1 },
-    { "jetbrains-clion", NULL, NULL, 1 << 4, 0, 0, 0, -1 },
-    { "Gimp", NULL, NULL, 1 << 5, 0, 0, 0, -1 },
-    { "qBittorrent", NULL, NULL, 1 << 6, 0, 0, 0, -1 },
+    { "jetbrains-idea-ce", NULL, NULL, 1 << 3, 0, 0, 0, -1 },
+    { "Gimp", NULL, NULL, 1 << 4, 0, 0, 0, -1 },
+    { "qBittorrent", NULL, NULL, 1 << 5, 0, 0, 0, -1 },
     { TERMCLASS, NULL, NULL, 0, 0, 1, 0, -1 },
     { NULL, NULL, "Event Tester", 0, 0, 0, 1, -1 },
     { NULL, "bg", NULL, 0, 0, 1, 1 << 7, -1 },
