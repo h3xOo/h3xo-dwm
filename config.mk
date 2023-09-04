ddfa9d441c001a191f5bbce4d4dbf27d1fc276a9
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-09-04 10:45:47 +0200

    Update config.mk

--- config.mk
+++ config.mk
@@ -28,8 +28,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-r
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c17 -pedantic -Wall -Wextra -Wno-deprecated-declarations -O2 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c17 -O2 -pedantic -Wall -Wextra -Wunused -Wno-deprecated-declarations ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
